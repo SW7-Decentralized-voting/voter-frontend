14cef1d5aeb3aca709bae6c1ac12f5ec1c4fbadd
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-10-02 11:22:32 +0200

    feat: Render candidates under party

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,32 @@ import './App.css'
 import './VotingPage.css'
 
 function App() {
-  const [selectedParty, setSelectedParty] = useState(null);
   const [parties, setParties] = useState([]);
-  const [partyCandidates, setPartyCandidates] = useState([]);
+  const [partyCandidates, setPartyCandidates] = useState({});
+  const [selectedParty, setSelectedParty] = useState(null);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
 
   const fetchParties = async () => {
     const parties = await getParties();
-    setParties(parties);
+    return parties;
   };
 
-  const fetchPartyCandidates = async () => {
-    const candidates = await getPartyCandidates();
-    setPartyCandidates(candidates);
+  const fetchPartyCandidates = async (partyId) => {
+    const candidates = await getPartyCandidates(partyId);
+    return candidates;
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      await fetchParties();
-      for (const party of parties) {
-        await fetchPartyCandidates(party.id);
+      const fetchedParties = await fetchParties();
+      setParties(fetchedParties);
+      
+      const candidatesData = {};
+      for (const party of fetchedParties) {
+        const candidates = await fetchPartyCandidates(party.id);
+        candidatesData[party.id] = candidates;
       }
+      setPartyCandidates(candidatesData);
     };
   
     fetchData();
@@ -32,30 +38,56 @@ function App() {
   const handleSelectParty = (event) => {
     const partyId = Number(event.target.value);
     setSelectedParty(partyId === selectedParty ? null : partyId);
+    setSelectedCandidate(null);
     console.log('Selected party:', partyId);
+  };
+
+  const handleSelectCandidate = (event) => {
+    const candidateId = Number(event.target.value);
+    setSelectedCandidate(candidateId === selectedCandidate ? null : candidateId);
+    setSelectedParty(null);
+    console.log('Selected candidate:', candidateId);
   };
 
   return (
     <>
-    <h1> Folketingsvalget 20xx</h1>
-    {parties.map((party) => (
-      <div className='party-container' key={party.id}> 
-        <label className='party-label'>
-          <input 
-            className='party-checkbox' 
-            type='checkbox' 
-            name='party' 
-            value={party.id} 
-            onChange={handleSelectParty} 
-            checked={selectedParty === party.id}
+      <h1>Folketingsvalget 20xx</h1>
+      {parties.map((party) => (
+        <div className='party-container' key={party.id}>
+          <label className='party-label'>
+            <input
+              className='party-checkbox'
+              type='checkbox'
+              name='party'
+              value={party.id}
+              onChange={handleSelectParty}
+              checked={selectedParty === party.id}
             />
-          {party.partyName}
-        </label>
-      </div>
-    
-    ))}
+            {party.partyName}
+          </label>
+          <div className='candidate-list'>
+            {partyCandidates[party.id] && partyCandidates[party.id].map((candidate) => (
+              <div className='candidate-contaier'>
+                <div key={candidate.id} className='candidate'>
+                  <label className='candidate-label'>
+                    <input
+                      className='candidate-checkbox'
+                      type='checkbox'
+                      name='candidate'
+                      value={candidate.id}
+                      onChange={handleSelectCandidate}
+                      checked={selectedCandidate === candidate.id}
+                    />
+                    {candidate.full_name}
+                  </label>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
 export default App
