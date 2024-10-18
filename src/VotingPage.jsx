ad5fdd8f52007ea39f0dfe8b8d4d7eb77a86106f
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-10-18 11:07:45 +0200

    feat: Explanatory text on digital ballot

--- src/VotingPage.jsx
+++ src/VotingPage.jsx
@@ -65,6 +65,9 @@ function VotingPage() {
     return (
       <>
         <h1>Folketingsvalget 20xx</h1>
+        <h3>Sæt X i rubrikken til venstre for et partinavn eller et kandidatnavn.</h3>
+        <p>Du kan kun sætte ét X på stemmesedlen.</p>
+        {parties.length === 0 && <p>Indlæser...</p>}
         {parties.map((party) => (
           <div className='party-container' key={party.id}>
             <label className='party-label'>
