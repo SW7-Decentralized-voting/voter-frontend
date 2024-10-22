d39c01323f50e5bd8c48b2898336ec67b620d629
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-22 02:50:36 +0200

    feat: cypress setup + first tests

---
+++ __test__/cypress/e2e/votingPage.cy.js
@@ -0,0 +1,42 @@
+describe('Voting Page Layout', () => {
+	const apiBaseUrl = Cypress.env('BACKEND_URL');
+	beforeEach(() => {
+		cy.intercept('GET', apiBaseUrl + '/voting', {});
+		cy.intercept('GET', apiBaseUrl + '/parties', { fixture: 'parties.json' });
+
+		cy.intercept('GET', apiBaseUrl + '/candidates?party=1', { fixture: 'candidates.json' });
+		cy.intercept('GET', apiBaseUrl + '/candidates?party=2', { fixture: 'candidates2.json' });
+		cy.intercept('GET', apiBaseUrl + '/candidates?party=3', { fixture: 'candidates3.json' });
+		cy.intercept('GET', apiBaseUrl + '/candidates?party=4', { fixture: 'candidates4.json' });
+		cy.intercept('GET', apiBaseUrl + '/candidates?party=5', { fixture: 'candidates5.json' });
+	});
+
+
+	it('should correctly display the voting page', () => {
+		cy.visit('/voting');
+		cy.get('div .header > h1').should('have.text', 'Folketingsvalget 20xx');
+	});
+
+	it('should display the parties', () => {
+		const parties = ['Nordlisten', 'Sydlisten', 'Østpartiet', 'Vestpartiet', 'Uden for partierne'];
+
+		cy.visit('/voting');
+		cy.get('div .party-container').should('have.length', 5).as('parties');
+		cy.get('@parties').each((party, index) => {
+			cy.wrap(party).children().eq(0).should('have.text', parties[index]);
+		});
+	});
+
+	it('should display the candidates for each party', async () => {
+		const candidates = await cy.fixture('candidatesAll.json');
+
+		console.log('Candidates (cypress):', candidates);
+
+		cy.visit('/voting');
+		cy.get('div .candidate-list').as('candidates');
+		cy.get('@candidates').each((candidateList, index) => {
+			Cypress.log(candidateList)
+			cy.wrap(candidateList).children().should('have.length', candidates.filter((candidate) => candidate.party === index + 1).length);
+		});
+	});
+});
