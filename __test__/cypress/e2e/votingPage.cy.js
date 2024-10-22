a081130eebb0fabe0668d040921d46ab875a0352
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-22 11:13:23 +0200

    fix: added wait for party fetching

--- __test__/cypress/e2e/votingPage.cy.js
+++ __test__/cypress/e2e/votingPage.cy.js
@@ -2,7 +2,7 @@ describe('Voting Page Layout', () => {
 	const apiBaseUrl = Cypress.env('BACKEND_URL');
 	beforeEach(() => {
 		cy.intercept('GET', apiBaseUrl + '/voting', {});
-		cy.intercept('GET', apiBaseUrl + '/parties', { fixture: 'parties.json' });
+		cy.intercept('GET', apiBaseUrl + '/parties', { fixture: 'parties.json' }).as('getParties');
 
 		cy.intercept('GET', apiBaseUrl + '/candidates?party=1', { fixture: 'candidates.json' });
 		cy.intercept('GET', apiBaseUrl + '/candidates?party=2', { fixture: 'candidates2.json' });
@@ -19,8 +19,8 @@ describe('Voting Page Layout', () => {
 
 	it('should display the parties', () => {
 		const parties = ['Nordlisten', 'Sydlisten', 'Østpartiet', 'Vestpartiet', 'Uden for partierne'];
-
 		cy.visit('/voting');
+		cy.wait('@getParties');
 		cy.get('div .party-container').should('have.length', 5).as('parties');
 		cy.get('@parties').each((party, index) => {
 			cy.wrap(party).children().eq(0).should('have.text', parties[index]);
