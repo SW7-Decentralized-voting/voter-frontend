c53168c4009ee310bf895c7309f1644d4dada428
Mafusn <Magnus.peetz.holt@gmail.com>
2024-10-28 11:38:04 +0100

    Feat: Possible to enlarge text in voting page

--- src/Screens/VotingPage.jsx
+++ src/Screens/VotingPage.jsx
@@ -1,103 +1,108 @@
-import { useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getPartyCandidates, getParties, voteForCandidate, voteForParty, voteBlank } from '../API/VotingAPI';
 import './VotingPage.css';
 
 function VotingPage() {
-    const navigate = useNavigate();
-    const [parties, setParties] = useState([]);
-    const [partyCandidates, setPartyCandidates] = useState({});
-    const [selectedParty, setSelectedParty] = useState(null);
-    const [selectedCandidate, setSelectedCandidate] = useState(null);
-    const hasFetchedData = useRef(false);
-  
-    const fetchParties = async () => {
-      const parties = await getParties();
-      return parties;
-    };
-  
-    const fetchPartyCandidates = async (partyId) => {
-      const candidates = await getPartyCandidates(partyId);
-      return candidates;
-    };
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        const fetchedParties = await fetchParties();
-        console.log('Parties:', fetchedParties);
-        setParties(fetchedParties);
-        
-        const candidatesData = {};
-        for (const party of fetchedParties) {
-          const candidates = await fetchPartyCandidates(party._id);
-          candidatesData[party._id] = candidates;
-          console.log('Candidates for party', party._id, candidates);
-        }
-        setPartyCandidates(candidatesData);
-        hasFetchedData.current = true;
-      };
-    
-      fetchData();
-    }, []);
-  
-    const handleSelectParty = (event) => {
-      const partyId = event.target.value;
-      setSelectedParty(partyId === selectedParty ? null : partyId);
-      setSelectedCandidate(null);
-      console.log('Selected party:', partyId);
-    };
-  
-    const handleSelectCandidate = (event) => {
-      const candidateId = event.target.value;
-      setSelectedCandidate(candidateId === selectedCandidate ? null : candidateId);
-      setSelectedParty(null);
-      console.log('Selected candidate:', candidateId);
-    };
-    
-    const handleCastVote = async () => {
-      if (selectedParty) {
-        await voteForParty(selectedParty);
-      } else if (selectedCandidate) {
-        await voteForCandidate(selectedCandidate);
-      } else {
-        voteBlank();
+  const navigate = useNavigate();
+  const [parties, setParties] = useState([]);
+  const [partyCandidates, setPartyCandidates] = useState({});
+  const [selectedParty, setSelectedParty] = useState(null);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const [isTextLarge, setIsTextLarge] = useState(false); // New state variable
+  const hasFetchedData = useRef(false);
+
+  const fetchParties = async () => {
+    const parties = await getParties();
+    return parties;
+  };
+
+  const fetchPartyCandidates = async (partyId) => {
+    const candidates = await getPartyCandidates(partyId);
+    return candidates;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedParties = await fetchParties();
+      console.log('Parties:', fetchedParties);
+      setParties(fetchedParties);
+
+      const candidatesData = {};
+      for (const party of fetchedParties) {
+        const candidates = await fetchPartyCandidates(party._id);
+        candidatesData[party._id] = candidates;
+        console.log('Candidates for party', party._id, candidates);
       }
-      setSelectedParty(null);
-      setSelectedCandidate(null);
-      sessionStorage.setItem('verified', 'false');
-      navigate('/login');
+      setPartyCandidates(candidatesData);
+      hasFetchedData.current = true;
     };
-  
-    return (
-      <>
-        <div className="header">
-          <h1>Folketingsvalget 20xx</h1>
-          <div className="upper-right-text">Opstillingskreds</div>
-        </div>
-        <h3>Sæt X i rubrikken til venstre for et partinavn eller et kandidatnavn.</h3>
-        <p>Du kan kun sætte ét X på stemmesedlen.</p>
-        {parties.length === 0 && <p>Indlæser...</p>}
-        {parties.map((party) => (
-          <div className='party-container' key={party._id}>
-            <label className='party-label'>
-              <input
-                className='party-checkbox'
-                type='checkbox'
-                name='party'
-                value={party._id}
-                onChange={handleSelectParty}
-                checked={selectedParty === party._id}
-              />
-              {party.name}
-            </label>
-            <div className='candidate-list'>
-              {partyCandidates[party._id] && partyCandidates[party._id].map((candidate) => (
-                <div key={candidate._id} className='candidate'>
-                  <label className='candidate-label'>
+
+    fetchData();
+  }, []);
+
+  const handleSelectParty = (event) => {
+    const partyId = event.target.value;
+    setSelectedParty(partyId === selectedParty ? null : partyId);
+    setSelectedCandidate(null);
+    console.log('Selected party:', partyId);
+  };
+
+  const handleSelectCandidate = (event) => {
+    const candidateId = event.target.value;
+    setSelectedCandidate(candidateId === selectedCandidate ? null : candidateId);
+    setSelectedParty(null);
+    console.log('Selected candidate:', candidateId);
+  };
+
+  const handleCastVote = async () => {
+    if (selectedParty) {
+      await voteForParty(selectedParty);
+    } else if (selectedCandidate) {
+      await voteForCandidate(selectedCandidate);
+    } else {
+      voteBlank();
+    }
+    setSelectedParty(null);
+    setSelectedCandidate(null);
+    sessionStorage.setItem('verified', 'false');
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1>Folketingsvalget 20xx</h1>
+        <div className="upper-right-text">Opstillingskreds</div>
+      </div>
+      <button className="text-size-button" onClick={() => setIsTextLarge(!isTextLarge)}>
+        {isTextLarge ? 'Normal tekststørrelse' : 'Forstør tekst'}
+      </button>
+      <h3 className={`${isTextLarge ? 'large-text' : ''}`}>Sæt X i rubrikken til venstre for et partinavn eller et kandidatnavn.</h3>
+      <p className={`${isTextLarge ? 'large-text' : ''}`}>Du kan kun sætte ét X på stemmesedlen.</p>
+      {parties.length === 0 && <p>Indlæser...</p>}
+      {parties.map((party) => (
+        <div className="party-container" key={party._id}>
+          <label className={`party-label ${isTextLarge ? 'large-text' : ''}`}>
+            <input
+              className="party-checkbox"
+              type="checkbox"
+              name="party"
+              value={party._id}
+              onChange={handleSelectParty}
+              checked={selectedParty === party._id}
+            />
+            {party.name}
+          </label>
+          <div className="candidate-list">
+            {partyCandidates[party._id] &&
+              partyCandidates[party._id].map((candidate) => (
+                <div key={candidate._id} className="candidate">
+                  <label className={`candidate-label ${isTextLarge ? 'large-text' : ''}`}>
                     <input
-                      className='candidate-checkbox'
-                      type='checkbox'
-                      name='candidate'
+                      className="candidate-checkbox"
+                      type="checkbox"
+                      name="candidate"
                       value={candidate._id}
                       onChange={handleSelectCandidate}
                       checked={selectedCandidate === candidate._id}
@@ -106,14 +111,18 @@ function VotingPage() {
                   </label>
                 </div>
               ))}
-            </div>
           </div>
-        ))}
-        <button className='submit-button' disabled={!selectedParty && !selectedCandidate} onClick={handleCastVote}>
-          Stem
-        </button>
-      </>
-    );
-  }
+        </div>
+      ))}
+      <button
+        className="submit-button"
+        disabled={!selectedParty && !selectedCandidate}
+        onClick={handleCastVote}
+      >
+        Stem
+      </button>
+    </>
+  );
+}
 
 export default VotingPage;
