2eacfea8528b6624874cbb2f267ca7a9b0a0c790
FantaSkink <45458221+Fantaskink@users.noreply.github.com>
2024-10-31 14:17:53 +0100

    feat: voting page, cypress, API functions, and placeholder key verification page (#7)
    * Set up minimal react project
    
    * Delete docker-compose.yaml
    
    * fix: Ignore .env
    
    * feat: Add getCandidates API function
    
    * feat: Implement voting page that fetches and displays each party.
    
    * feat: Selecting a checkbox now deselects all other checkboxes.
    
    * feat: Render candidates under party
    
    * feat: Add cast vote button
    
    * lint: Linting setup
    
    * feat: Add voting API functions
    
    * feat: Add hash verification page
    
    * fix: Replace mock data and apply linter
    
    * feat: Create verifyHash API function
    
    * fix: Rename votingAPI.jsx to VotingAPI.jsx
    
    * feat[CI]: Added workflow files + test script
    
    * feat: Specify port to run frontend on
    
    * feat: Explanatory text on digital ballot
    
    * fix: Parity with backend data
    
    * fix: Change ballot to light mode and fix selection logic
    
    * feat: Opstillingskreds in upper right corner
    
    * Feat: /voting is now protected
    
    * Refactor
    
    * feat: Setup of Cypress testing (#3)
    
    * feat: cypress setup + first tests
    
    * feat: added workflow file for cypress tests
    
    * fix: changed backend url in workflow file (cypress)
    
    * fix: removed cypress workflow file for now
    
    * fix: cypress lint setup (+ linting)
    
    * Feat: Added verifyKeyPage to verify the given keys
    
    * Small fixes
    
    * Feat: Verify key for voting is now in danish
    
    * Feat: Bigger text for entering key
    
    * Feat: Possible to enlarge text in voting page
    
    * Feat: Bigger text in login page
    
    * Feat: Possible to vote blank
    
    * Feat: Possible to vote using only keyboard
    
    * Fix: Lint is working
    
    * feat: made dockerfile and docker-compose
    
    * fix: updated cypress test to use actual values
    
    * fix: remove docker-compose.yml from workflow
    
    feat: docker compose action test
    
    fix: removed unnecessary cd command
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    Update docker-compose.yml
    
    * Cypress tilbage...
    
    * Så er alle ikke-orbline glade
    
    * Fix: Now linter is happy
    
    * fix: Prevent undefined error for parties
    
    * fix: Add undefined check to conditional rendering
    
    ---------
    
    Co-authored-by: Jacob Terpe Woods <jacobterpewoods@gmail.com>
    Co-authored-by: Mafusn <Magnus.peetz.holt@gmail.com>
    Co-authored-by: Mafusn <92912046+Mafusn@users.noreply.github.com>

---
+++ __test__/cypress/e2e/votingPage.cy.js
@@ -0,0 +1,33 @@
+function enterKey() {
+	cy.get('.key-input').type('password');
+	cy.get('.key-input').type('{enter}');
+	cy.visit('/voting');
+}
+
+describe('Voting Page Layout', () => {
+	// const apiBaseUrl = Cypress.env('BACKEND_URL');
+	beforeEach(() => {
+		cy.visit('/login');
+		enterKey();
+	});
+
+
+	it('should correctly display the voting page', () => {
+		cy.get('div .header > h1').should('have.text', 'Folketingsvalget 20xx');
+	});
+
+	it('should display the parties', () => {
+		const parties = ['Nordlisten', 'Sydlisten', 'Østpartiet', 'Vestpartiet', 'Uden for partierne'];
+		cy.visit('/voting');
+		cy.get('div .party-container').should('have.length', 5).as('parties');
+		cy.get('@parties').each((party, index) => {
+			cy.wrap(party).children().eq(0).should('have.text', parties[index]);
+		});
+	});
+
+	it('should display the candidates for each party', () => {
+		cy.visit('/voting');
+		cy.get('div .candidate-list').as('candidates');
+		cy.get('@candidates').should('have.length.above', 0);
+	});
+});
---
+++ src/Screens/VotingPage.jsx
@@ -0,0 +1,135 @@
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getPartyCandidates, getParties, voteForCandidate, voteForParty, voteBlank } from '../API/VotingAPI';
+import './VotingPage.css';
+
+function VotingPage() {
+  const navigate = useNavigate();
+  const [parties, setParties] = useState([]);
+  const [partyCandidates, setPartyCandidates] = useState({});
+  const [selectedParty, setSelectedParty] = useState(null);
+  const [selectedCandidate, setSelectedCandidate] = useState(null);
+  const [isTextLarge, setIsTextLarge] = useState(false); // New state variable
+  const hasFetchedData = useRef(false);
+
+  const fetchParties = async () => {
+    const parties = await getParties();
+    return parties;
+  };
+
+  const fetchPartyCandidates = async (partyId) => {
+    const candidates = await getPartyCandidates(partyId);
+    return candidates;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedParties = await fetchParties();
+      setParties(fetchedParties);
+
+      const candidatesData = {};
+      for (const party of fetchedParties) {
+        const candidates = await fetchPartyCandidates(party._id);
+        candidatesData[party._id] = candidates;
+      }
+      setPartyCandidates(candidatesData);
+      hasFetchedData.current = true;
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSelectParty = (event) => {
+    const partyId = event.target.value;
+    setSelectedParty(partyId === selectedParty ? null : partyId);
+    setSelectedCandidate(null);
+  };
+
+  const handleSelectCandidate = (event) => {
+    const candidateId = event.target.value;
+    setSelectedCandidate(candidateId === selectedCandidate ? null : candidateId);
+    setSelectedParty(null);
+  };
+
+  const handleCastVote = async () => {
+    if (selectedParty) {
+      await voteForParty(selectedParty);
+    } else if (selectedCandidate) {
+      await voteForCandidate(selectedCandidate);
+    } else {
+      voteBlank();
+    }
+    setSelectedParty(null);
+    setSelectedCandidate(null);
+    sessionStorage.setItem('verified', 'false');
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1>Folketingsvalget 20xx</h1>
+        <div className="upper-right-text">Opstillingskreds</div>
+      </div>
+      <button className="text-size-button" onClick={() => setIsTextLarge(!isTextLarge)}>
+        {isTextLarge ? 'Normal tekststørrelse' : 'Forstør tekst'}
+      </button>
+      <h3 className={`${isTextLarge ? 'large-text' : ''}`}>Sæt X i rubrikken til venstre for et partinavn eller et kandidatnavn.</h3>
+      <p className={`${isTextLarge ? 'large-text' : ''}`}>Du kan kun sætte ét X på stemmesedlen.</p>
+      {(parties?.length ?? 0) === 0 && <p>Indlæser...</p>}
+      {parties?.map((party) => (
+        <div className="party-container" key={party._id}>
+          <label className={`party-label ${isTextLarge ? 'large-text' : ''}`}>
+            <input
+              className="party-checkbox"
+              type="checkbox"
+              name="party"
+              value={party._id}
+              onChange={handleSelectParty}
+              checked={selectedParty === party._id}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleSelectParty(e);
+                  selectedParty === party._id ? e.preventDefault() : null;
+                }
+              }}
+            />
+            {party.name}
+          </label>
+          <div className="candidate-list">
+            {partyCandidates[party._id] &&
+              partyCandidates[party._id].map((candidate) => (
+                <div key={candidate._id} className="candidate">
+                  <label className={`candidate-label ${isTextLarge ? 'large-text' : ''}`}>
+                    <input
+                      className="candidate-checkbox"
+                      type="checkbox"
+                      name="candidate"
+                      value={candidate._id}
+                      onChange={handleSelectCandidate}
+                      checked={selectedCandidate === candidate._id}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          handleSelectCandidate(e);
+                          selectedCandidate === candidate._id ? e.preventDefault() : null;
+                        }
+                      }}
+                    />
+                    {candidate.name}
+                  </label>
+                </div>
+              ))}
+          </div>
+        </div>
+      ))}
+      <button
+        className="submit-button"
+        onClick={handleCastVote}
+      >
+        Stem
+      </button>
+    </>
+  );
+}
+
+export default VotingPage;
