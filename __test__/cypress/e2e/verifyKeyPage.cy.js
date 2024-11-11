e01b9319c3318dcd68388b11111a269cb3e7bdeb
TheMOHD3886 <65400638+TheMOHD3886@users.noreply.github.com>
2024-11-11 11:19:05 +0100

    fix[test]: updated verifyKeyPage tests
    - added tests for entering valid and invalid keys

--- __test__/cypress/e2e/verifyKeyPage.cy.js
+++ __test__/cypress/e2e/verifyKeyPage.cy.js
@@ -1,29 +1,47 @@
 describe('VerifyKeyPage Layout', () => {
   beforeEach(() => {
     cy.visit('/login');
+
+    cy.intercept('GET', Cypress.env('BACKEND_URL') + '/pollingStations/*', {
+      statusCode: 200,
+      body: {
+        pollingStation: '6720c9073b17ad2d0922002e',
+        port: 3001
+      },
+    })
+  });
+  
+ 
+  it('should display an error message for an invalid key', () => {
+    // Intercept the API request before triggering the action
+    cy.intercept('POST', Cypress.env('BACKEND_URL') + '/key/verify', {
+      statusCode: 401,
+      body: {
+        error: 'Invalid key'
+      }
+    }).as('verifyInvalidKey');
+
+    // Enter an invalid key
+    cy.get('.key-input').type('invalid-key');
+    cy.get('.key-button').click();
+
+    // Wait for the API call and check for error message
+    cy.contains('Forkert nøgle, prøv venligst igen.').should('be.visible');
   });
 
-  it('should display the page header', () => {
-    cy.get('.verify-key-page-div h1').should('be.visible').and('contain', 'Indtast nøgle');
-  });
+  it('should navigate to the voting page for a valid key', () => {
+    // Mock the API response for valid key verification
+    cy.intercept('POST', Cypress.env('BACKEND_URL') + '/key/verify', {
+      statusCode: 200,
+      body: { token: 'valid-jwt-token' },
+    }).as('verifyValidKey');
 
-  it('should display the instructions paragraph', () => {
-    cy.get('.enter-key-text')
-      .should('be.visible')
-      .and('contain', 'Indtast nøglen som De har modtaget ved skranken.');
-  });
+    // Enter a valid key
+    cy.get('.key-input').type('valid-key');
+    cy.get('.key-button').click();
 
-  it('should display the key input field with placeholder text', () => {
-    cy.get('.key-input')
-      .should('be.visible')
-      .and('have.attr', 'placeholder', 'Indtast din nøgle her');
-  });
-
-  it('should display the "Log ind" button', () => {
-    cy.get('.key-button').should('be.visible').and('contain', 'Log ind');
-  });
-
-  it('should display the "Verificer din stemme" button', () => {
-    cy.get('.verify-button').should('be.visible').and('contain', 'Verificer din stemme');
+    // Wait for the API call and check for navigation
+    cy.url().should('include', '/voting');
+    cy.window().its('sessionStorage').invoke('getItem', 'jwt').should('eq', 'valid-jwt-token');
   });
 });
