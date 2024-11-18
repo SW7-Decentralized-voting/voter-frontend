0398ae0ca1cc95aaec8a5888326cabb6518414ef
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-18 14:31:23 +0100

    fix: voting is sent to blockchain

--- src/API/VotingAPI.jsx
+++ src/API/VotingAPI.jsx
@@ -4,6 +4,10 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
+const blockchainApi = axios.create({
+    baseURL: import.meta.env.VITE_BLOCKCHAIN_API_URL,
+});
+
 const getPartyCandidates = async (partyId) => {
     const response = await api.get('/candidates?party=' + partyId);
     return response.data;
@@ -14,19 +18,16 @@ const getParties = async () => {
     return response.data;
 };
 
-const voteForCandidate = async (candidateId) => {
-    const response = await api.post('/vote/candidate', { candidateId });
+const vote = async (id) => {
+    const body = { id: id };
+    const response = await blockchainApi.post('/vote', body, {
+        headers: {
+            Authorization: sessionStorage.getItem('jwt'),
+        }
+    });
+    // eslint-disable-next-line no-console
+    console.log(response.data);
     return response.data;
 };
 
-const voteForParty = async (partyId) => {
-    const response = await api.post('/vote/party', { partyId });
-    return response.data;
-};
-
-const voteBlank = async () => {
-    const response = await api.post('/vote/blank');
-    return response.data;
-};
-
-export { getPartyCandidates, getParties, voteForCandidate, voteForParty, voteBlank };
+export { getPartyCandidates, getParties, vote };
